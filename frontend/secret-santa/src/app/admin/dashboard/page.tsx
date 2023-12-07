cc761090cd8db44617c56fb48c2cd66d25e0d49d
Tibor Kadar <kadartibor24@gmail.com>
2023-12-07 21:49:33 +0100

    add admin dashboard page

---
+++ frontend/secret-santa/src/app/admin/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function DashboardComponent() {
+    return (
+      <div className="flex justify-center items-center">
+        <div className="flex flex-row w-full">
+            <input type="text" placeholder="Name your santa session" className="grow input w-full" />
+        </div>
+      </div>
+    )
+  }
+  
